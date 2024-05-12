9608d240778a1b48bda843f4a6d5018b9e0d1cd3
aleksander <aleksandr.shumeiko@simbirsoft.com>
2024-05-12 11:48:02 +0300

    добавил транзакции к миграциям

--- local/php_interface/migrations/AddMail_20240510190203.php
+++ local/php_interface/migrations/AddMail_20240510190203.php
@@ -3,6 +3,11 @@
 namespace Sprint\Migration;
 
 
+use Throwable;
+use Bitrix\Main\Diag\Debug;
+use Bitrix\Main\Application;
+use Bitrix\Main\DB\SqlQueryException;
+
 class AddMail_20240510190203 extends Version
 {
     protected $description = '';
@@ -10,51 +15,82 @@ class AddMail_20240510190203 extends Version
     protected $moduleVersion = '4.6.1';
 
     /**
-     * @return void
-     * @throws Exceptions\HelperException
+     * @return bool
+     * @throws SqlQueryException
      */
-    public function up(): void
+    public function up(): bool
     {
-        $helper = $this->getHelperManager();
-        $helper->Event()->saveEventType('GEO_IP_ERROR', [
-            'LID' => 'ru',
-            'EVENT_TYPE' => 'email',
-            'NAME' => 'Ошибка GeoIp',
-            'DESCRIPTION' => '',
-            'SORT' => '150',
-        ]);
-        $helper->Event()->saveEventMessage('GEO_IP_ERROR', [
-            'LID' => [
-                0 => 's1',
-            ],
-            'ACTIVE' => 'Y',
-            'EMAIL_FROM' => '#DEFAULT_EMAIL_FROM#',
-            'EMAIL_TO' => 'it@geoip.com',
-            'SUBJECT' => 'Ошибка GeoIP',
-            'MESSAGE' => 'Произошла ошибка при работе GeoIp:
+        $conn = Application::getConnection();
+        $conn->startTransaction();
+
+        try {
+            $helper = $this->getHelperManager();
+            $helper->Event()->saveEventType('GEO_IP_ERROR', [
+                'LID' => 'ru',
+                'EVENT_TYPE' => 'email',
+                'NAME' => 'Ошибка GeoIp',
+                'DESCRIPTION' => '',
+                'SORT' => '150',
+            ]);
+            $helper->Event()->saveEventMessage('GEO_IP_ERROR', [
+                'LID' => [
+                    0 => 's1',
+                ],
+                'ACTIVE' => 'Y',
+                'EMAIL_FROM' => '#DEFAULT_EMAIL_FROM#',
+                'EMAIL_TO' => 'it@geoip.com',
+                'SUBJECT' => 'Ошибка GeoIP',
+                'MESSAGE' => 'Произошла ошибка при работе GeoIp:
 #TEXT#',
-            'BODY_TYPE' => 'text',
-            'BCC' => '',
-            'REPLY_TO' => '',
-            'CC' => '',
-            'IN_REPLY_TO' => '',
-            'PRIORITY' => '',
-            'FIELD1_NAME' => '',
-            'FIELD1_VALUE' => '',
-            'FIELD2_NAME' => '',
-            'FIELD2_VALUE' => '',
-            'SITE_TEMPLATE_ID' => '',
-            'ADDITIONAL_FIELD' => [
-            ],
-            'LANGUAGE_ID' => 'ru',
-            'EVENT_TYPE' => '[ GEO_IP_ERROR ] Ошибка GeoIp',
-        ]);
+                'BODY_TYPE' => 'text',
+                'BCC' => '',
+                'REPLY_TO' => '',
+                'CC' => '',
+                'IN_REPLY_TO' => '',
+                'PRIORITY' => '',
+                'FIELD1_NAME' => '',
+                'FIELD1_VALUE' => '',
+                'FIELD2_NAME' => '',
+                'FIELD2_VALUE' => '',
+                'SITE_TEMPLATE_ID' => '',
+                'ADDITIONAL_FIELD' => [
+                ],
+                'LANGUAGE_ID' => 'ru',
+                'EVENT_TYPE' => '[ GEO_IP_ERROR ] Ошибка GeoIp',
+            ]);
+
+            $conn->commitTransaction();
+        } catch(Throwable $e) {
+            Debug::dump($e->getMessage());
+            $conn->rollbackTransaction();
+
+            return false;
+        }
+
+        return true;
     }
 
-    public function down(): void
+    /**
+     * @throws SqlQueryException
+     */
+    public function down(): bool
     {
-        $helper = $this->getHelperManager();
-        $helper->Event()->deleteEventMessage(['SUBJECT' => 'Ошибка GeoIP', 'EVENT_NAME' => 'GEO_IP_ERROR']);
-        $helper->Event()->deleteEventType(['LID' => 'ru', 'EVENT_NAME' => 'GEO_IP_ERROR']);
+        $conn = Application::getConnection();
+        $conn->startTransaction();
+
+        try {
+            $helper = $this->getHelperManager();
+            $helper->Event()->deleteEventMessage(['SUBJECT' => 'Ошибка GeoIP', 'EVENT_NAME' => 'GEO_IP_ERROR']);
+            $helper->Event()->deleteEventType(['LID' => 'ru', 'EVENT_NAME' => 'GEO_IP_ERROR']);
+
+            $conn->commitTransaction();
+        } catch (Throwable $e) {
+            Debug::dump($e->getMessage());
+            $conn->rollbackTransaction();
+
+            return false;
+        }
+
+        return true;
     }
 }
--- local/php_interface/migrations/IpListAdd_20240508073322.php
+++ local/php_interface/migrations/IpListAdd_20240508073322.php
@@ -3,6 +3,11 @@
 namespace Sprint\Migration;
 
 
+use Throwable;
+use Bitrix\Main\Diag\Debug;
+use Bitrix\Main\Application;
+use Bitrix\Main\DB\SqlQueryException;
+
 class IpListAdd_20240508073322 extends Version
 {
     protected $description = '';
@@ -13,190 +18,221 @@ class IpListAdd_20240508073322 extends Version
 
     /**
      * @return void
-     * @throws Exceptions\HelperException
+     * @throws SqlQueryException
      */
-    public function up(): void
+    public function up(): bool
     {
-        $helper = $this->getHelperManager();
-        $hlblockId = $helper->Hlblock()->saveHlblock([
-            'NAME' => self::HL_BLOCK_NAME,
-            'TABLE_NAME' => 'ip_list',
-            'LANG' => [
-                'ru' => [
-                    'NAME' => 'Список IP',
+        $conn = Application::getConnection();
+        $conn->startTransaction();
+
+        try {
+            $helper = $this->getHelperManager();
+            $hlblockId = $helper->Hlblock()->saveHlblock([
+                'NAME' => self::HL_BLOCK_NAME,
+                'TABLE_NAME' => 'ip_list',
+                'LANG' => [
+                    'ru' => [
+                        'NAME' => 'Список IP',
+                    ],
+                    'en' => [
+                        'NAME' => 'Ip List',
+                    ],
                 ],
-                'en' => [
-                    'NAME' => 'Ip List',
+            ]);
+            $helper->Hlblock()->saveField($hlblockId, [
+                'FIELD_NAME' => 'UF_IP',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_IP',
+                'SORT' => '100',
+                'MULTIPLE' => 'N',
+                'MANDATORY' => 'N',
+                'SHOW_FILTER' => 'S',
+                'SHOW_IN_LIST' => 'Y',
+                'EDIT_IN_LIST' => 'Y',
+                'IS_SEARCHABLE' => 'Y',
+                'SETTINGS' => [
+                    'SIZE' => 20,
+                    'ROWS' => 1,
+                    'REGEXP' => '',
+                    'MIN_LENGTH' => 0,
+                    'MAX_LENGTH' => 0,
+                    'DEFAULT_VALUE' => '',
                 ],
-            ],
-        ]);
-        $helper->Hlblock()->saveField($hlblockId, [
-            'FIELD_NAME' => 'UF_IP',
-            'USER_TYPE_ID' => 'string',
-            'XML_ID' => 'UF_IP',
-            'SORT' => '100',
-            'MULTIPLE' => 'N',
-            'MANDATORY' => 'N',
-            'SHOW_FILTER' => 'S',
-            'SHOW_IN_LIST' => 'Y',
-            'EDIT_IN_LIST' => 'Y',
-            'IS_SEARCHABLE' => 'Y',
-            'SETTINGS' => [
-                'SIZE' => 20,
-                'ROWS' => 1,
-                'REGEXP' => '',
-                'MIN_LENGTH' => 0,
-                'MAX_LENGTH' => 0,
-                'DEFAULT_VALUE' => '',
-            ],
-            'EDIT_FORM_LABEL' => [
-                'en' => 'IP',
-                'ru' => 'IP',
-            ],
-            'LIST_COLUMN_LABEL' => [
-                'en' => 'IP',
-                'ru' => 'IP',
-            ],
-            'LIST_FILTER_LABEL' => [
-                'en' => 'IP',
-                'ru' => 'IP',
-            ],
-            'ERROR_MESSAGE' => [
-                'en' => '',
-                'ru' => '',
-            ],
-            'HELP_MESSAGE' => [
-                'en' => '',
-                'ru' => '',
-            ],
-        ]);
-        $helper->Hlblock()->saveField($hlblockId, [
-            'FIELD_NAME' => 'UF_CITY',
-            'USER_TYPE_ID' => 'string',
-            'XML_ID' => 'UF_CITY',
-            'SORT' => '200',
-            'MULTIPLE' => 'N',
-            'MANDATORY' => 'N',
-            'SHOW_FILTER' => 'S',
-            'SHOW_IN_LIST' => 'Y',
-            'EDIT_IN_LIST' => 'Y',
-            'IS_SEARCHABLE' => 'Y',
-            'SETTINGS' => [
-                'SIZE' => 20,
-                'ROWS' => 1,
-                'REGEXP' => '',
-                'MIN_LENGTH' => 0,
-                'MAX_LENGTH' => 0,
-                'DEFAULT_VALUE' => '',
-            ],
-            'EDIT_FORM_LABEL' => [
-                'en' => 'City',
-                'ru' => 'Город',
-            ],
-            'LIST_COLUMN_LABEL' => [
-                'en' => 'City',
-                'ru' => 'Город',
-            ],
-            'LIST_FILTER_LABEL' => [
-                'en' => 'City',
-                'ru' => 'Город',
-            ],
-            'ERROR_MESSAGE' => [
-                'en' => '',
-                'ru' => '',
-            ],
-            'HELP_MESSAGE' => [
-                'en' => '',
-                'ru' => '',
-            ],
-        ]);
-        $helper->Hlblock()->saveField($hlblockId, [
-            'FIELD_NAME' => 'UF_REGION',
-            'USER_TYPE_ID' => 'string',
-            'XML_ID' => 'UF_REGION',
-            'SORT' => '300',
-            'MULTIPLE' => 'N',
-            'MANDATORY' => 'N',
-            'SHOW_FILTER' => 'S',
-            'SHOW_IN_LIST' => 'Y',
-            'EDIT_IN_LIST' => 'Y',
-            'IS_SEARCHABLE' => 'Y',
-            'SETTINGS' => [
-                'SIZE' => 20,
-                'ROWS' => 1,
-                'REGEXP' => '',
-                'MIN_LENGTH' => 0,
-                'MAX_LENGTH' => 0,
-                'DEFAULT_VALUE' => '',
-            ],
-            'EDIT_FORM_LABEL' => [
-                'en' => 'Region',
-                'ru' => 'Регион',
-            ],
-            'LIST_COLUMN_LABEL' => [
-                'en' => 'Region',
-                'ru' => 'Регион',
-            ],
-            'LIST_FILTER_LABEL' => [
-                'en' => 'Region',
-                'ru' => 'Регион',
-            ],
-            'ERROR_MESSAGE' => [
-                'en' => '',
-                'ru' => '',
-            ],
-            'HELP_MESSAGE' => [
-                'en' => '',
-                'ru' => '',
-            ],
-        ]);
-        $helper->Hlblock()->saveField($hlblockId, [
-            'FIELD_NAME' => 'UF_COUNTRY',
-            'USER_TYPE_ID' => 'string',
-            'XML_ID' => 'UF_COUNTRY',
-            'SORT' => '400',
-            'MULTIPLE' => 'N',
-            'MANDATORY' => 'N',
-            'SHOW_FILTER' => 'S',
-            'SHOW_IN_LIST' => 'Y',
-            'EDIT_IN_LIST' => 'Y',
-            'IS_SEARCHABLE' => 'Y',
-            'SETTINGS' => [
-                'SIZE' => 20,
-                'ROWS' => 1,
-                'REGEXP' => '',
-                'MIN_LENGTH' => 0,
-                'MAX_LENGTH' => 0,
-                'DEFAULT_VALUE' => '',
-            ],
-            'EDIT_FORM_LABEL' => [
-                'en' => 'Country',
-                'ru' => 'Страна',
-            ],
-            'LIST_COLUMN_LABEL' => [
-                'en' => 'Country',
-                'ru' => 'Страна',
-            ],
-            'LIST_FILTER_LABEL' => [
-                'en' => 'Country',
-                'ru' => 'Страна',
-            ],
-            'ERROR_MESSAGE' => [
-                'en' => '',
-                'ru' => '',
-            ],
-            'HELP_MESSAGE' => [
-                'en' => '',
-                'ru' => '',
-            ],
-        ]);
+                'EDIT_FORM_LABEL' => [
+                    'en' => 'IP',
+                    'ru' => 'IP',
+                ],
+                'LIST_COLUMN_LABEL' => [
+                    'en' => 'IP',
+                    'ru' => 'IP',
+                ],
+                'LIST_FILTER_LABEL' => [
+                    'en' => 'IP',
+                    'ru' => 'IP',
+                ],
+                'ERROR_MESSAGE' => [
+                    'en' => '',
+                    'ru' => '',
+                ],
+                'HELP_MESSAGE' => [
+                    'en' => '',
+                    'ru' => '',
+                ],
+            ]);
+            $helper->Hlblock()->saveField($hlblockId, [
+                'FIELD_NAME' => 'UF_CITY',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_CITY',
+                'SORT' => '200',
+                'MULTIPLE' => 'N',
+                'MANDATORY' => 'N',
+                'SHOW_FILTER' => 'S',
+                'SHOW_IN_LIST' => 'Y',
+                'EDIT_IN_LIST' => 'Y',
+                'IS_SEARCHABLE' => 'Y',
+                'SETTINGS' => [
+                    'SIZE' => 20,
+                    'ROWS' => 1,
+                    'REGEXP' => '',
+                    'MIN_LENGTH' => 0,
+                    'MAX_LENGTH' => 0,
+                    'DEFAULT_VALUE' => '',
+                ],
+                'EDIT_FORM_LABEL' => [
+                    'en' => 'City',
+                    'ru' => 'Город',
+                ],
+                'LIST_COLUMN_LABEL' => [
+                    'en' => 'City',
+                    'ru' => 'Город',
+                ],
+                'LIST_FILTER_LABEL' => [
+                    'en' => 'City',
+                    'ru' => 'Город',
+                ],
+                'ERROR_MESSAGE' => [
+                    'en' => '',
+                    'ru' => '',
+                ],
+                'HELP_MESSAGE' => [
+                    'en' => '',
+                    'ru' => '',
+                ],
+            ]);
+            $helper->Hlblock()->saveField($hlblockId, [
+                'FIELD_NAME' => 'UF_REGION',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_REGION',
+                'SORT' => '300',
+                'MULTIPLE' => 'N',
+                'MANDATORY' => 'N',
+                'SHOW_FILTER' => 'S',
+                'SHOW_IN_LIST' => 'Y',
+                'EDIT_IN_LIST' => 'Y',
+                'IS_SEARCHABLE' => 'Y',
+                'SETTINGS' => [
+                    'SIZE' => 20,
+                    'ROWS' => 1,
+                    'REGEXP' => '',
+                    'MIN_LENGTH' => 0,
+                    'MAX_LENGTH' => 0,
+                    'DEFAULT_VALUE' => '',
+                ],
+                'EDIT_FORM_LABEL' => [
+                    'en' => 'Region',
+                    'ru' => 'Регион',
+                ],
+                'LIST_COLUMN_LABEL' => [
+                    'en' => 'Region',
+                    'ru' => 'Регион',
+                ],
+                'LIST_FILTER_LABEL' => [
+                    'en' => 'Region',
+                    'ru' => 'Регион',
+                ],
+                'ERROR_MESSAGE' => [
+                    'en' => '',
+                    'ru' => '',
+                ],
+                'HELP_MESSAGE' => [
+                    'en' => '',
+                    'ru' => '',
+                ],
+            ]);
+            $helper->Hlblock()->saveField($hlblockId, [
+                'FIELD_NAME' => 'UF_COUNTRY',
+                'USER_TYPE_ID' => 'string',
+                'XML_ID' => 'UF_COUNTRY',
+                'SORT' => '400',
+                'MULTIPLE' => 'N',
+                'MANDATORY' => 'N',
+                'SHOW_FILTER' => 'S',
+                'SHOW_IN_LIST' => 'Y',
+                'EDIT_IN_LIST' => 'Y',
+                'IS_SEARCHABLE' => 'Y',
+                'SETTINGS' => [
+                    'SIZE' => 20,
+                    'ROWS' => 1,
+                    'REGEXP' => '',
+                    'MIN_LENGTH' => 0,
+                    'MAX_LENGTH' => 0,
+                    'DEFAULT_VALUE' => '',
+                ],
+                'EDIT_FORM_LABEL' => [
+                    'en' => 'Country',
+                    'ru' => 'Страна',
+                ],
+                'LIST_COLUMN_LABEL' => [
+                    'en' => 'Country',
+                    'ru' => 'Страна',
+                ],
+                'LIST_FILTER_LABEL' => [
+                    'en' => 'Country',
+                    'ru' => 'Страна',
+                ],
+                'ERROR_MESSAGE' => [
+                    'en' => '',
+                    'ru' => '',
+                ],
+                'HELP_MESSAGE' => [
+                    'en' => '',
+                    'ru' => '',
+                ],
+            ]);
+
+            $conn->commitTransaction();
+        } catch (Throwable $e) {
+            Debug::dump($e->getMessage());
+            $conn->rollbackTransaction();
+
+            return false;
+        }
+
+        return true;
     }
 
-    public function down(): void
+    /**
+     * @throws SqlQueryException
+     */
+    public function down(): bool
     {
-        $helper = $this->getHelperManager();
-        $helper->Hlblock()->deleteHlblock(
-            $helper->Hlblock()->getHlblockId(self::HL_BLOCK_NAME)
-        );
+        $conn = Application::getConnection();
+        $conn->startTransaction();
+
+        try {
+            $helper = $this->getHelperManager();
+            $helper->Hlblock()->deleteHlblock(
+                $helper->Hlblock()->getHlblockId(self::HL_BLOCK_NAME)
+            );
+
+            $conn->commitTransaction();
+        } catch (Throwable $e) {
+            Debug::dump($e->getMessage());
+            $conn->rollbackTransaction();
+
+            return false;
+        }
+
+        return true;
     }
 }
